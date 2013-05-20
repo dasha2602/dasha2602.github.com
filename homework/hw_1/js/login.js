7faafa42a0e02552c9d5da900cd469dca7533550
dasha2602 <ds.260293@rambler.ru>
2013-05-20 16:01:18 +0400

    clean comments

--- homework/hw_1/js/login.js
+++ homework/hw_1/js/login.js
@@ -1,29 +1,22 @@
 jQuery(document).ready(function() {
     function fHide(){
-        $('#login').fadeOut(200);
+        //$('#login').fadeOut(200);
+        $('#login').slideUp(200);
         $('#dark').css("visibility", "hidden");
+        $('#page').removeClass('blur');
     }
     $('#openLogin').on('click', function() {
         $('#page').addClass('blur');
         $('#dark').css('visibility', 'visible');
-        //$('#login').show();
-        //$('#login').show('scale');
-        //$('#login').animate({'top':'-10px'},100);
-        $('#login').fadeIn(200); 
+        //$('#login').fadeIn(200);   
+        $('#login').slideDown(200);
 
     });
     $('#cancel').on('click', function(){
-        //$('#login').animate({'top':'-10px'},100);
-        fHide();
-        $('#page').removeClass('blur');
+        fHide();  
     });
      $('#submit').on('click', function(event) {
         event.preventDefault();
         fHide();
     });
-     
-    //$('.main').text('dfgvd');
-    /*$("#box").on( "click", function() {
-        $( this ).css( "width", "+=200" );
-    });*/
 });
